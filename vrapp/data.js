207c1a8203c0d7cbe94cecf5e872d7f040bedf22
tpradeeppai <6568064+tpradeeppai@users.noreply.github.com>
2020-04-03 22:53:18 +0530

    Add files via upload

---
+++ vrapp/data.js
@@ -0,0 +1,151 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-led0lyb-360-panorama-view-miami-downtownjpeg",
+      "name": "DIAT-view1.jpeg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8367240666977249,
+          "pitch": -0.07601053061475582,
+          "rotation": 0,
+          "target": "1-tpv0cjd-360-panorama-pier-1jpeg"
+        },
+        {
+          "yaw": 2.431553450668524,
+          "pitch": -0.0076519137238566515,
+          "rotation": 18.06415775814132,
+          "target": "2-lk110lu8-360-panorama-view-bridgejpeg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8433888955527333,
+          "pitch": 0.02742785433085615,
+          "title": "Diat - move 1",
+          "text": "move1"
+        },
+        {
+          "yaw": 2.46005829288295,
+          "pitch": 0.1914881599701168,
+          "title": "DIAT - move2",
+          "text": "move2"
+        }
+      ]
+    },
+    {
+      "id": "1-tpv0cjd-360-panorama-pier-1jpeg",
+      "name": "DIAT-view2.jpeg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5459169641871355,
+          "pitch": 0.0771487541799516,
+          "rotation": 5.497787143782138,
+          "target": "0-led0lyb-360-panorama-view-miami-downtownjpeg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5643092415421567,
+          "pitch": 0.20938779248811556,
+          "title": "Diat - go back",
+          "text": "go back"
+        }
+      ]
+    },
+    {
+      "id": "2-lk110lu8-360-panorama-view-bridgejpeg",
+      "name": "DIAT-view3.jpeg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1569,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.3139064139846415,
+          "pitch": 0.18032163737498763,
+          "title": "DIAT - return",
+          "text": "return"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
